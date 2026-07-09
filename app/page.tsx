750f66a807df2d1c30c3402f1a25e8d168a19366
Vincent Chen <vshchen@gmail.com>
2026-07-09 20:49:47 +0800

    project initialization

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+    {/* this is comment */}
+    <center><h1> Hello My Dear </h1>
+    <h2>My name is Vincent</h2></center>
+    </>
+  );
+}
+// jsx --> tsx, x means React
